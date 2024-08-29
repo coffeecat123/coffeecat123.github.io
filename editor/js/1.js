16a82e9c11a755a2cf2690f5f777b839025d03bd
coffeecat <coffeeccaatt@gmail.com>
2024-08-30 01:14:16 +0800

    2024/8/29

--- editor/js/1.js
+++ editor/js/1.js
@@ -12,6 +12,7 @@ window.onload = function () {
     txt.style.fontSize = font_size + "px";
 
     ok.onclick = () => {
+        console.log(window);
         if (window.self !== window.top) {
             window.parent.postMessage('REMOVE_IFRAME', '*');
             console.log('removed iframe');
