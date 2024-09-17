25f67fb5005e3323167d34650d836dd3ed205aff
coffeecat <coffeeccaatt@gmail.com>
2024-09-17 14:33:58 +0800

    2024/9/17

--- editor/js/1.js
+++ editor/js/1.js
@@ -23,15 +23,18 @@ window.onload = function () {
             txt.style.whiteSpace = "pre";
         }
         w ^= 1;
+        saveTextareaContent();
     };
 
     big.onclick = () => {
         font_size += 2;
         txt.style.fontSize = font_size + "px";
+        saveTextareaContent();
     }
     sml.onclick = () => {
         font_size -= 2;
         txt.style.fontSize = font_size + "px";
+        saveTextareaContent();
     }
     txt.addEventListener('keydown', function (e) {
         if (e.keyCode === 9) {
@@ -47,8 +50,70 @@ window.onload = function () {
             this.selectionStart = this.selectionEnd = start + 1;
             e.preventDefault();
         }
+        saveTextareaContent();
     }, false);
+    /*
     window.addEventListener('message', function (e) {
         txt.value = e.data;
     });
+    */
+    const dbName = 'cftDB';
+    const storeName = 'txt';
+    let db;
+
+    // 打开或创建数据库
+    const openRequest = indexedDB.open(dbName, 1);
+
+    openRequest.onupgradeneeded = (event) => {
+        db = event.target.result;
+        // 创建对象存储（Object Store），主键为 'id'
+        if (!db.objectStoreNames.contains(storeName)) {
+            db.createObjectStore(storeName, { keyPath: 'id' });
+        }
+    };
+
+    openRequest.onsuccess = (event) => {
+        db = event.target.result;
+        console.log('Database opened successfully');
+        // 尝试从数据库中加载内容
+        loadTextareaContent();
+    };
+
+    openRequest.onerror = (event) => {
+        console.error('Error opening database:', event.target.error);
+    };
+
+    // 保存数据到 IndexedDB
+    function saveTextareaContent() {
+        const transaction = db.transaction([storeName], 'readwrite');
+        const store = transaction.objectStore(storeName);
+
+        store.put({ id: 1, content:txt.value ,font_size,w});
+    }
+
+    // 从 IndexedDB 加载数据到 textarea
+    function loadTextareaContent() {
+        const transaction = db.transaction([storeName], 'readonly');
+        const store = transaction.objectStore(storeName);
+        const request = store.get(1);
+
+        request.onsuccess = (event) => {
+            if (event.target.result) {
+                const data = event.target.result;
+                txt.value = data.content;
+                w = data.w;
+                font_size = data.font_size;
+                txt.style.fontSize = font_size + "px";
+                if (!w) {
+                    txt.style.whiteSpace = "pre-wrap";
+                } else {
+                    txt.style.whiteSpace = "pre";
+                }
+            }
+        };
+
+        request.onerror = (event) => {
+            console.error('Error loading content:', event.target.error);
+        };
+    }
 };
